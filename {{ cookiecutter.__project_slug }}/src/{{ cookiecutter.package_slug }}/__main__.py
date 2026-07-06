e15a1392902a435168c9757f9c7e4a1dc63e55ed
Johann Maximilian Christensen <johann.christensen@dlr.de>
2026-07-06 15:27:17 +0200

    feat: update package defaults

---
+++ {{ cookiecutter.__project_slug }}/src/{{ cookiecutter.package_slug }}/__main__.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: {{ cookiecutter.__year }} {{ cookiecutter.author_name }}
+#
+# SPDX-License-Identifier: {{ cookiecutter.license }}
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
